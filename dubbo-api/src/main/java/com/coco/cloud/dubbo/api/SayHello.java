a45a63286a50baba50e38f804ba7f15a9f929724
liuqiang <liuqiang@ourdocker.cn>
2020-08-05 21:57:17 +0800

    spring-cloud-alibaba

---
+++ dubbo-api/src/main/java/com/coco/cloud/dubbo/api/SayHello.java
@@ -0,0 +1,12 @@
+package com.coco.cloud.dubbo.api;
+
+/**
+ * @author liuqiang@ourdocker.cn
+ * @version 0.0.1
+ * @date 2020/8/5 21:42
+ */
+public interface SayHello {
+
+    void hello();
+
+}
---
+++ dubbo-provider/src/main/java/com/coco/alibaba/dubbo/custom/DubboCustomApplication.java
@@ -0,0 +1,13 @@
+package com.coco.alibaba.dubbo.custom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboCustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DubboCustomApplication.class, args);
+    }
+
+}
